e5d5e1ca9e4b4cb9b3b71d2c38884ebd826680af
jonathan parisot <parisot.jonathan@gmail.com>
2017-02-13 18:54:25 +0100

    worked on sinatra but didn't solve it

--- app.rb
+++ app.rb
@@ -2,11 +2,30 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+# require_relative 'lib/cookbook'
+# require_relative 'lib/controller'
+# require_relative 'lib/router'
+set :bind, '0.0.0.0'
+
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
+require_relative "lib/cookbook"
+
 get '/' do
-  'Yo bro'
+  erb :index
+  @cookbook = Cookbook.new("lib/recipes.csv")
+  # @controller = Controller.new(@cookbook)
+  # @view = View.new
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/team/:username' do
+  puts params[:username]
+  "the username is #{params[:username]}"
 end
